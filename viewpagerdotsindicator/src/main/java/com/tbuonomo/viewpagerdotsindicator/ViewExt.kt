910153943fa9d82a95fb41dcd23d875f639805c7
Baha Can AYDIN <bahacan.aydin@ozan.com>
2022-01-23 12:29:56 +0300

    color binder added

--- viewpagerdotsindicator/src/main/java/com/tbuonomo/viewpagerdotsindicator/ViewExt.kt
+++ viewpagerdotsindicator/src/main/java/com/tbuonomo/viewpagerdotsindicator/ViewExt.kt
@@ -1,6 +1,9 @@
 package com.tbuonomo.viewpagerdotsindicator
 
 import android.view.View
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
 
 fun View.setPaddingHorizontal(padding: Int) {
   setPadding(padding, paddingTop, padding, paddingBottom)
@@ -8,4 +11,12 @@ fun View.setPaddingHorizontal(padding: Int) {
 
 fun View.setPaddingVertical(padding: Int) {
   setPadding(paddingLeft, padding, paddingRight, padding)
+}
+
+
+@BindingAdapter("allColor")
+fun DotsIndicator.allColor(@ColorRes colorRes: Int) {
+    val color = ContextCompat.getColor(context, colorRes)
+    this.dotsColor = color
+    this.selectedDotColor = color
 }
